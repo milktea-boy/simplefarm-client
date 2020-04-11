c5afe05cb5d6aac0bae29b5d87dfceafc41a89db
litiancheng <litiancheng136@163.com>
2020-04-12 02:12:46 +0800

    Home升级的测试

--- Assets/Scprits/Game/UI/Controller/PopupHomeController.cs
+++ Assets/Scprits/Game/UI/Controller/PopupHomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace GameScene {
 
@@ -8,6 +10,110 @@ namespace GameScene {
     /// 家弹窗控制器
     /// </summary>
     public class PopupHomeController : BaseController<PopupHomeModel,PopupHomeView> {
+
+        int farmLevel;//农场等级
+        int curBuildingLevel;//当前建筑等级
+        int curExp;//当前经验
+        int upgradeNeedExp;//升级所需经验
+        int curCoin;//当前金币
+        int upPrice;//升级所需金币
+
+        private void Awake()
+        {
+            base.Awake();
+
+            Model.GetFarmInfo(OnfarmInfoCallback);
+
+            Model.GetFarmDetailInfo(OnfarmDetailInfoCallback);
+
+            Model.GetUpgradeInfo(0,OnUpgradeInfoCallback);
+        }
+
+        private void OnDestroy()
+        {
+            
+        }
+
+        /// <summary>
+        /// 家建筑升级点击事件
+        /// </summary>
+        public void OnBtnUpgradeClick()
+        {
+            if (this.farmLevel > this.curBuildingLevel && this.curExp > this.upgradeNeedExp && this.curCoin > this.upPrice)
+            {
+                Model.Upgrade(0, OnUpgradeCallback);
+            }
+        }
+
+        private void OnfarmInfoCallback(Hashtable data)
+        {
+            bool success = bool.Parse(data["success"].ToString());
+            string message = data["message"].ToString();
+
+            if (!success)
+            {
+                PopupCommon.GetSingleton().ShowView(message);
+                return;
+            }
+
+            string curLevel = data["buildInfo"].ToString();
+
+            this.curBuildingLevel = int.Parse(curLevel);
+            //ArrayList buildInfoArrayList = data["buildInfo"]; //先跳过
+
+        }
+
+        private void OnfarmDetailInfoCallback(Hashtable data)
+        {
+            bool success = bool.Parse(data["success"].ToString());
+            string message = data["message"].ToString();
+            string userID = data["userId"].ToString();
+            string nickName = data["nickname"].ToString();
+            string level = data["level"].ToString();
+            string exp = data["exp"].ToString();
+            string needExp = data["needExp"].ToString();
+            string coin = data["coin"].ToString();
+
+            if (!success)
+            {
+                PopupCommon.GetSingleton().ShowView(message);
+                return;
+            }
+
+            this.farmLevel = int.Parse(level);
+            this.curExp = int.Parse(exp);
+            this.upgradeNeedExp = int.Parse(needExp);
+            this.curCoin = int.Parse(coin);
+        }
+
+        private void OnUpgradeInfoCallback(Hashtable data)
+        {
+            bool success = bool.Parse(data["success"].ToString());
+            string message = data["message"].ToString();
+            string price = data["message"].ToString();
+
+            if (!success)
+            {
+                PopupCommon.GetSingleton().ShowView(message);
+                return;
+            }
+
+            this.upPrice = int.Parse(price);
+        }
+
+        private void OnUpgradeCallback(Hashtable data)
+        {
+            bool success = bool.Parse(data["success"].ToString());
+            string message = data["message"].ToString();
+
+            if (!success)
+            {
+                PopupCommon.GetSingleton().ShowView(message);
+                return;
+            }
+
+            //升级完成后的后续逻辑
+        }
 
         public void OnBtnCloseClick()
         {
--- Assets/Scprits/Game/UI/Model/PopupHomeModel.cs
+++ Assets/Scprits/Game/UI/Model/PopupHomeModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,100 @@ namespace GameScene {
     /// 家弹窗模型
     /// </summary>
     public class PopupHomeModel : MonoBehaviour {
+
+        private Action<Hashtable> farmInfoCallback;
+        private Action<Hashtable> farmDetailInfoCallback;
+        private Action<Hashtable> upgradeCallback;
+        private Action<Hashtable> upgradeInfoCallback;
+
+        private void Awake()
+        {
+            SocketServer.GetSingleton().AddListener("global/farmInfo", OnUpgradeCallback);
+            SocketServer.GetSingleton().AddListener("global/farmDetailInfo",OnUpgradeCallback);
+            SocketServer.GetSingleton().AddListener("global/upgradeBuildInfo", OnUpgradeCallback);
+            SocketServer.GetSingleton().AddListener("global/upgradeBuild", OnUpgradeCallback);
+        }
+
+        private void OnDestroy()
+        {
+            SocketServer.GetSingleton().RemoveListener("global/farmInfo");
+            SocketServer.GetSingleton().RemoveListener("global/farmDetailInfo");
+            SocketServer.GetSingleton().RemoveListener("global/upgradeBuildInfo");
+            SocketServer.GetSingleton().RemoveListener("global/upgradeBuild");
+        }
+
+        /// <summary>
+        /// 获取农场信息
+        /// </summary>
+        /// <param name="callback"></param>
+        public void GetFarmInfo(Action<Hashtable> callback)
+        {
+            farmInfoCallback = callback;
+            SocketServer.GetSingleton().Send("global/farmInfo", new object[] { });
+        }
+
+        /// <summary>
+        /// 获取农场详细信息
+        /// </summary>
+        public void GetFarmDetailInfo(Action<Hashtable> callback)
+        {
+            farmDetailInfoCallback = callback;
+            SocketServer.GetSingleton().Send("global/farmDetailInfo", new object[] { });
+        }
+
+        /// <summary>
+        /// 获取建筑升级信息
+        /// </summary>
+        /// <param name="buildID">建筑ID</param>
+        /// <param name="callback">完成回调</param>
+        public void GetUpgradeInfo(int buildID,Action<Hashtable> callback)
+        {
+            upgradeInfoCallback = callback;
+            SocketServer.GetSingleton().Send("global/upgradeBuildInfo", new object[] { buildID});
+        }
+
+        /// <summary>
+        /// 升级建筑
+        /// </summary>
+        /// <param name="buildID">建筑ID</param>
+        /// <param name="callback">完成回调</param>
+        public void Upgrade(int buildID,Action<Hashtable> callback)
+        {
+            upgradeCallback = callback;
+            SocketServer.GetSingleton().Send("global/upgradeBuild",new object[] { buildID});
+        }
+
+        private void OnfarmInfoCallback(Hashtable data)
+        {
+            if (farmInfoCallback != null)
+            {
+                farmInfoCallback(data);
+            }
+        }
+
+        private void OnfarmDetailInfoCallback(Hashtable data)
+        {
+            if (farmDetailInfoCallback != null)
+            {
+                farmDetailInfoCallback(data);
+            }
+        }
+
+        private void OnUpgradeCallback(Hashtable data)
+        {
+            if (upgradeCallback != null)
+            {
+                upgradeCallback(data);
+            }
+        }
+
+        private void OnUpgradeInfoCallback(Hashtable data)
+        {
+            if (upgradeInfoCallback != null)
+            {
+                upgradeInfoCallback(data);
+            }
+        }
 
     }
 }
